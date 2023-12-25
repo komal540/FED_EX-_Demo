4ac7d82d8a1e9631ee0a8bb60d19c2d12cad1c54
Komal Vashistha <70580214+komal540@users.noreply.github.com>
2023-12-25 02:58:13 -0800

    Add files via upload

---
+++ src/main/java/fedex/demo/domain/OutsourcedPart.java
@@ -0,0 +1,29 @@
+package fedex.demo.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ *
+ *
+ *
+ *
+ */
+@Entity
+@DiscriminatorValue("2")
+public class OutsourcedPart extends Part{
+String companyName;
+
+    public OutsourcedPart(){
+        this.maxInv = 500;
+        this.minInv = 0;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
